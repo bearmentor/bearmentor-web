cf4593a5827912316f2d8252a47fc5d114e9a489
M Haidar Hanif <git@mhaidarhanif.com>
2024-10-02 07:03:13 +0700

    feat: configure remix & root

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,19 @@
+import { type LinksFunction, type MetaFunction } from "@remix-run/node"
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+  useRouteError,
+} from "@remix-run/react"
 
-import "~/tailwind.css";
+import "~/tailwind.css"
+
+import { configSite } from "~/configs/site"
+import { createMeta } from "~/utils/meta"
+
+export const meta: MetaFunction = () => createMeta({})
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -20,11 +26,11 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-];
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang={configSite.languageCode}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -39,9 +45,34 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError()
+
+  console.error(error)
+
+  return (
+    <html lang={configSite.languageCode}>
+      <head>
+        <title>Sorry, something wrong</title>
+        <Meta />
+        <Links />
+      </head>
+
+      <body>
+        <main className="p-2">
+          <p>Sorry, something went wrong. Please try again later.</p>
+          <p>Regards, Bearmentor</p>
+        </main>
+
+        <Scripts />
+      </body>
+    </html>
+  )
 }
